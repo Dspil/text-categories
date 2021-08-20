e606aae7793344950bd17305b60bf03e9121c524
Dspil <dennisspiliopoylos@gmail.com>
2021-08-20 12:59:41 +0300

    package essentials added

--- typing-categories.el
+++ typing-categories.el
@@ -1,3 +1,30 @@
+;;; typing-categories.el --- Typing in different categories to allow editing of seamingly unrelated file positions                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Dionisios Spiliopoulos
+
+;; Author: Dionisios Spiliopoulos
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Put a description of the package here
+
+;;; Code:
+
 ;; variables
 
 (defvar typing-category-buffer-prefix "~typing_categories::" "The prefix of typing-category buffers.")
@@ -106,3 +133,7 @@
 (global-set-key (kbd "C-x t m") 'change-typing-category)
 (global-set-key (kbd "C-x t t") 'typing-category)
 (global-set-key (kbd "C-x t d") 'delete-typing-category)
+
+;(provide 'typing-modes)          
+                                  
+;;; typing-categories.el ends here
