746003538feaece54b7ea30af8add29f2eb8f76d
Dspil <dennisspiliopoylos@gmail.com>
2021-09-19 00:27:18 +0300

    larger commentary

--- typing-categories.el
+++ typing-categories.el
@@ -23,7 +23,9 @@
 
 ;;; Commentary:
 
-;; Annotates each character in the buffer with a category
+;; Annotates each character in the buffer with a category chosen by
+;; the user.  The user can delete all characters belonging to a
+;; certain category.
 
 ;;; Code:
 
